3e6a1c0365c5db85e3b3634b3bd4d4b512095b3b
10sr <8slashes+git@gmail.com>
2012-01-18 02:46:40 +0900

    seems to work. another old code remains.

--- dig-url.ks.js
+++ dig-url.ks.js
@@ -1,46 +1,36 @@
 var PLUGIN_INFO =
-<KeySnailPlugin>
+    <KeySnailPlugin>
     <name>Dig-url</name>
     <updateURL>https://raw.github.com/10sr/keysnail-plugin/master/dig-url.ks.js</updateURL>
     <description>dig url</description>
     <version>0.1</version>
     <author mail="" homepage="http://10sr.jottit.com/">10sr</author>
     <license>NYSL</license>
+    <minVersion>1.0</minVersion>
     <include>main</include>
     <detail><![CDATA[
-=== Overview ===
-Dig current url (or go up directory).
-For example, when viewing "http://example.com/foo/bar" you can go up to "http://example.com/foo" or "http://example.com".
-||<
-    ]]></detail>
-</KeySnailPlugin>;
-function dig2(){
+            === Usage ===
+            Dig current url (or go up directory).
+            For example, when viewing http://example.com/foo/bar you can go up to http://example.com/foo or http://example.com.
+            ]]></detail>
+    </KeySnailPlugin>;
+
+function digURL(){
     var url = window.content.location.href;
-    var h = window.content.location.hash;
-    var p = window.content.location.protocol
-    var nsurl = [];
-    var pname = "";
-    var ssurl = [];
-    var durl = [];
-    nsurl = url.split("#");
-    var pname = nsurl[1];
-    ssurl = nsurl[0].split("/");
-    durl[0] = ssurl[0] + "//" + ssurl[2];
-    ssurl.splice(0,3);
-    for (var i = 0; i < ssurl.length; i++){
-        var durlsaved = durl[0];
-        durl.unshift(durlsaved + "/" + ssurl[i]);
-    };
-    if (pname) {
-        var durlfull = durl[0] + "#"+ pname;
-        durl.unshift(durlfull);
-    };
+    var re = /(\/|#)/;          // paren cannot be removed!
+    var durl = [url,];
+    var elem = url.split(re);
+    for(var i = elem.length - 1; i > 4; i--){
+        if (elem[i-1].match(re) || elem[i-1] == ""){ continue; }
+        durl.push(elem.slice(0, i).join(""));
+    }
     prompt.selector({ message : "dig " + url,
                       collection : durl,
                       callback : function (i) { window.content.location.href = durl[i]; },
                     });
 }
-function dig(){
+
+function dig2(){
     var url = window.content.location.href;
     var durl = [url,];
     var s = url;
@@ -48,12 +38,13 @@ function dig(){
         if(s.match(/:\/\/[^/]+\/?$/)){ break; }
         s = s.replace(/(\/|#)[^/#]+\/?$/, "");
         durl[i] = s;
-    };
+    }
     prompt.selector({ message : "dig " + url,
                       collection : durl,
                       callback : function (i) { window.content.location.href = durl[i]; },
                     });
 }
+
 plugins.withProvides(function (provide) {
-    provide("dig-url", dig, "dig url");
+    provide("dig-url", digURL, "dig url with selector");
 }, PLUGIN_INFO);
