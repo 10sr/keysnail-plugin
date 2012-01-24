7e07989b7befbf4319d2270f87a67c288a3312bd
10sr <8slashes+git@gmail.com>
2012-01-24 12:53:23 +0900

    fix bug

--- instapaper.ks.js
+++ instapaper.ks.js
@@ -60,9 +60,9 @@ function post(tab, cm){
                                                                 "&title=" +
                                                                 encodeURIComponent(title));
                           // url = "javascript:function%20iprl5(){var%20d=document,z=d.createElement('scr'+'ipt'),b=d.body,l=d.location;try{if(!b)throw(0);d.title='(Saving...)%20'+d.title;z.setAttribute('src',l.protocol+'//www.instapaper.com/j/mt8YO6Cuosmf?u='+encodeURIComponent(l.href)+'&t='+(new%20Date().getTime()));b.appendChild(z);}catch(e){alert('Please%20wait%20until%20the%20page%20has%20loaded.');}}iprl5();void(0)";
-                          }
                       }
-                  });
+                  }
+                 );
 }
 
 plugins.withProvides(function (provide) {
