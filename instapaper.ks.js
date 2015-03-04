3ee42ca7013ce02a182c4d5f9fe6de9fb3e1e6db
10sr <8slashes+git@gmail.com>
2015-03-04 17:54:24 +0900

    Fix to work

--- instapaper.ks.js
+++ instapaper.ks.js
@@ -3,7 +3,7 @@ var PLUGIN_INFO =
     <name>instapaper</name>
     <updateURL>https://raw.github.com/10sr/keysnail-plugin/master/instapaper.ks.js</updateURL>
     <description>Post current page to instapaper</description>
-    <version>0.3.1</version>
+    <version>0.3.2</version>
     <author mail="" homepage="http://10sr.github.com/">10sr</author>
     <license>NYSL</license>
     <minVersion>1.8.3</minVersion>
@@ -32,7 +32,7 @@ function postTab(tab, cm){
   var password = "";
   var passwordManager = (Cc["@mozilla.org/login-manager;1"].
                          getService(Ci.nsILoginManager));
-  var logins = passwordManager.findLogins({}, "http://www.instapaper.com",
+  var logins = passwordManager.findLogins({}, "https://www.instapaper.com",
                                           "", null);
   for (var i = 0; i < logins.length; i++) {
     if (logins[i].username != "") {
@@ -56,7 +56,7 @@ function postTab(tab, cm){
     },
     callback : function(xhr){
       display.echoStatusBar(xhr.status);
-      if (xhr.readyState == 4 && xhr.status == 201) {
+      if (xhr.readyState === 4 && Math.floor(xhr.status / 100) === 2) {
         display.showPopup("Instapaper", "Page \"" + title +
                           "\" added successfully.");
         display.echoStatusBar("Instapaper: adding \"" + url +
@@ -64,8 +64,9 @@ function postTab(tab, cm){
         plugins.options["instapaper.close_after_post"] &&
           gBrowser.removeTab(tab);
       }else{
-        display.echoStatusBar(
-          "Instapaper: Something wrong has happended!"
+        display.showPopup(
+          "Instapaper",
+          xhr.status.toString() + " " + xhr.statusText
         );
         gBrowser.selectedTab = gBrowser.addTab(
           "http://www.instapaper.com/edit?url=" +
